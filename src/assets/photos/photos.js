ba1b9f3a752c8a373b1f845348cf24272f5324e5
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2024-11-11 01:09:26 +0700

    Add photos in collections with mansory layout

---
+++ src/assets/photos/photos.js
@@ -0,0 +1,70 @@
+// import photos
+import photos1 from './photos1.jpg'
+import photos2 from './photos2.jpg'
+import photos3 from './photos3.jpg'
+import photos4 from './photos4.jpg'
+import photos5 from './photos5.jpg'
+import photos6 from './photos6.jpg'
+import photos7 from './photos7.jpg'
+import photos8 from './photos8.jpg'
+import photos9 from './photos9.jpg'
+import photos10 from './photos10.jpg'
+import photos11 from './photos11.jpg'
+import photos12 from './photos12.jpg'
+import photos13 from './photos13.jpg'
+import photos14 from './photos14.jpg'
+import photos15 from './photos15.jpg'
+import photos16 from './photos16.jpeg'
+import photos17 from './photos17.png'
+import photos18 from './photos18.jpg'
+import photos19 from './photos19.jpg'
+import photos20 from './photos20.jpg'
+import photos21 from './photos21.jpg'
+import photos22 from './photos22.jpg'
+import photos23 from './photos23.jpg'
+import photosA from './photosA.png'
+import photosB from './photosB.jpg'
+import photosC from './photosC.jpg'
+import photosD from './photosD.jpg'
+import photosE from './photosE.jpg'
+import photosF from './photosF.jpg'
+import photosG from './photosG.jpg'
+import photosH from './photosH.jpg'
+import photosI from './photosI.jpg'
+import photosJ from './photosJ.jpg'
+import photosK from './photosK.jpg'
+
+const photos = [
+    photosC,
+    photos1,
+    photos2,
+    photos3,
+    photosA,
+    photos4,
+    photos17,
+    photos5,
+    photosB,
+    photos6,
+    photos7,
+    photosD,
+    photos8,
+    photosF,
+    photos9,
+    photosG,
+    photos10,
+    photos11,
+    photos21,
+    photosH,
+    photos12,
+    photos13,
+    photosI,
+    photos14,
+    photos15,
+    photos16,
+    photosJ,
+    photos22,
+    photos20, 
+    photos17, 
+]
+
+export default photos;
--- src/components/GalleryFillters/Collections/Photos.jsx
+++ src/components/GalleryFillters/Collections/Photos.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Masonry from 'react-masonry-css';
+import styles from './collections.module.scss';
+import photos from '../../../assets/photos/photos';
 
 export default function Photos() {
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+    };
+
     return (
-        <div>Photos</div>
-    )
+        <div className={styles.galleryContainer}>
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className={styles.myMasonryGrid}
+                columnClassName={styles.myMasonryGridColumn}
+            >
+                {photos.map((photo, index) => (
+                    <div key={index} className={styles.galleryItem}>
+                        <img src={photo} alt={`Photo ${index + 1}`} className={styles.photo} />
+                    </div>
+                ))}
+            </Masonry>
+        </div>
+    );
 }
